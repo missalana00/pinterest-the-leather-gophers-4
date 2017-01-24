cc855355cd46f60cf55edde929d4cb6e1895fafc
j-west <westja89@gmail.com>
2017-01-24 11:59:47 -0600

    Remove unused function

--- app/controllers/first.js
+++ app/controllers/first.js
@@ -38,14 +38,10 @@ app.controller("FirstCtrl", function($scope, getFactory, $mdDialog) {
       $mdDialog.hide();
     };
 
-
     $scope.cancel = function() {
       $mdDialog.cancel();
     };
 
-    $scope.answer = function(answer) {
-      $mdDialog.hide(answer);
-    };
   }
 });
 
