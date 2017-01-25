473dc1d88486057696e617c5459b6ecb64872a19
Jared Fuller <jaredshane@gmail.com>
2017-01-25 15:51:57 -0600

    login button working as well

--- app/factories/authFactory.js
+++ app/factories/authFactory.js
@@ -1,7 +1,7 @@
 app.factory('authFactory', function () {
   return {
     login: (email, password) => {
-      firebase.auth().signInWIthEmailandPassword(email, password)
+      firebase.auth().signInWithEmailAndPassword(email, password)
     },
     register: (email, password) => {
       firebase.auth().createUserWithEmailAndPassword(email, password)
