8ca5ce32cc56a2901d60167b3a30fb3143a722f2
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-26 11:27:49 -0600

    only loads in boards of current user

--- app/factories/boardArrayFactory.js
+++ app/factories/boardArrayFactory.js
@@ -7,23 +7,28 @@ app.factory('boardArrayFactory',  function($http, getFactory){
         return getFactory.getBoards()
         .then((val)=>{
         let newArray = []
-
+        let currentUser = firebase.auth().currentUser.uid
 
         for(var key in val.data){
           // console.log ("key, ", key)
           // console.log("val.data", val.data)
-          let boardObj = {
-            "name" : val.data[key].name,
-            "title" : val.data[key].title
-          }
+          console.log("uid of board", val.data[key].uid )
+          if(val.data[key].uid === currentUser){
+            let boardObj = {
+              "name" : val.data[key].name,
+              "title" : val.data[key].title,
+              "uid" : val.data[key].uid
+            }
+
+          // if(boardObj.uid === currentUser){
           newArray.push(boardObj)
-
+          }
         }
-
+        console.log(newArray)
         return newArray
 
 
-      }) //end of then
+      })//end of then
     }// end of boardArray()
   }; // end of factory object
 })// end of factory
