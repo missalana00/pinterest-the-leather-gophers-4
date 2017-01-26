4d70ca0ffcfd4cd8af0398b3837f21cb0ba70404
Jared Fuller <jaredshane@gmail.com>
2017-01-26 11:33:03 -0600

    added a resolve to show or hide the logout button based on if the user is logged in or not

--- app/config.js
+++ app/config.js
@@ -14,22 +14,42 @@ app.config(($routeProvider, $locationProvider, $mdIconProvider) => {
     });
 
 
+  const showHideLogout = {
+    showHideLogout: function() {
+       const authReady = firebase.auth().onAuthStateChanged(user => {
+         authReady()
+           if (!user) {
+             $('.logoutButton').addClass('ng-hide')
+           } else if (user) {
+             $('.logoutButton').removeClass("ng-hide")
+           }
+
+      }) //authReady
+
+    }
+  } //checkForAuth
+
+
   $routeProvider
     .when("/", {
       controller: "FirstCtrl",
       templateUrl: "app/partials/firstpartial.html",
+      resolve: showHideLogout
     })
     .when("/login", {
       controller: "SecondCtrl",
       templateUrl: "app/partials/secondpartial.html",
+      resolve: showHideLogout
     })
     .when("/profile", {
       controller: "ThirdCtrl",
       templateUrl: "app/partials/thirdpartial.html",
+      resolve: showHideLogout
     })
     .when("/boards/:boardName", {
       controller: "FourthCtrl",
-      templateUrl: "app/partials/fourthpartial.html"
+      templateUrl: "app/partials/fourthpartial.html",
+      resolve: showHideLogout
     })
     .otherwise({
       redirectTo: "/"
