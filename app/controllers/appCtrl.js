6fea03dbe54fbee31e76fedef0712b2b88a68a7f
Sorrel Brigman <sorrel.k@gmail.com>
2017-01-26 15:17:35 -0600

    filters boards by user

--- app/controllers/appCtrl.js
+++ app/controllers/appCtrl.js
@@ -1,9 +1,11 @@
 app.controller("AppCtrl", function($scope) {
   console.log("AppCtrl")
 
+
+
   function AppCtrl($scope) {
     $scope.currentNavItem = 'page1';
     $scope.open = false;
   }
-  
+
 });
