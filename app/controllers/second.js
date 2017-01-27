89626eb02abcb17f5df367752eb39de67337b1a9
Jared Fuller <jaredshane@gmail.com>
2017-01-26 23:10:18 -0600

    removed some console logs that had been added into second.js

--- app/controllers/second.js
+++ app/controllers/second.js
@@ -1,7 +1,6 @@
 
 
 app.controller("SecondCtrl", function($scope, authFactory, $location, toastFactory) {
-  console.log("SecondCtrl")
 
   $scope.loginButton = function (e, p) {
     var email = e
@@ -22,8 +21,7 @@ app.controller("SecondCtrl", function($scope, authFactory, $location, toastFacto
 
     var email = e
     var password = p
-    console.log(e)
-    console.log(p)
+
 
     authFactory.register(email, password)
     .then(()=>{
@@ -45,23 +43,18 @@ app.controller("SecondCtrl", function($scope, authFactory, $location, toastFacto
     firebase.auth().signInWithPopup(provider).then(function(result) {
       // This gives you a GitHub Access Token. You can use it to access the GitHub API.
       var token = result.credential.accessToken;
-      console.log("token", token);
       // The signed-in user info.
       var user = result.user;
-      console.log("user", user);
+
       // ...
     }).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
-      console.log("errorCode", errorCode);
       var errorMessage = error.message;
-      console.log("errorMessage", errorMessage);
       // The email of the user's account used.
       var email = error.email;
-      console.log("email", email);
       // The firebase.auth.AuthCredential type that was used.
       var credential = error.credential;
-      console.log("credential", credential);
       // ...
     });
   }
